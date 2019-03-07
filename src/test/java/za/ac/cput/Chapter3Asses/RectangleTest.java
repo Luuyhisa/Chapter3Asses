bff89318d193cd3fd09225f4f3f95028c4adec8d
Lungisa Lucky Mkhasakhasa <216060117@mycput.ac.za>
2019-03-08 00:04:19 +0200

    Using Maven as a build tool  i. Object Equality  ii. Object Identity  iii. Failing Test  iv.Timeouts  v. DisablingTest

--- src/test/java/za/ac/cput/Chapter3Asses/RectangleTest.java
+++ src/test/java/za/ac/cput/Chapter3Asses/RectangleTest.java
@@ -1,29 +1,35 @@
-package za.ac.cput.Chapter3Asses;
-import org.junit.*;
-import org.junit.rules.Timeout;
+    package za.ac.cput.Chapter3Asses;
+    import org.junit.*;
+    import org.junit.rules.Timeout;
 
-public class RectangleTest {
-    Rectangle rect = new Rectangle(5,4);
-    int dExpectedArea = 20;
-    Rectangle rectE = new Rectangle(5,4);
-    @Test
-    public void area() {Assert.assertEquals(dExpectedArea,rect.area());
+    public class RectangleTest {
+        Rectangle rect = new Rectangle(5,4);
+        int dExpectedArea = 20;
+        Rectangle rectE = new Rectangle(5,4);
+        @Test
+        public void area() {
+            Assert.assertEquals(dExpectedArea,rect.area());
+        }
+        @Test
+        public void perimeterObjectEquality() {
+            int dExpectedPerimeter=18;
+            Assert.assertEquals(dExpectedPerimeter,rect.perimeter());}
+        @Test
+        public void perimeterObjectIdentity(){
+            Assert.assertNotSame(rectE,rect);
+        }
+        @Test
+        public void FailingTest(){
+            Assert.fail("Failing Test");
+        }
+        @Rule
+        public Timeout globalTimeout = new Timeout(20);
+        @Test
+        public void timeOutTest(){
+            int infi=1;while(infi==1){}}
+        @Ignore
+        @Test
+        public void ignoreTest(){
+            int infi=1;while(infi==1){}
+        }
     }
-    @Test
-    public void perimeterObjectEquality() {int dExpectedPerimeter=18;
-        Assert.assertEquals(dExpectedPerimeter,rect.perimeter());}
-    @Test
-    public void perimeterObjectIdentity() {Assert.assertNotSame(rectE,rect);
-    }
-    @Test
-    public void FailingTest(){Assert.fail("Failing Test");
-    }
-    @Rule
-    public Timeout globalTimeout = new Timeout(20);
-    @Test
-    public void timeOutTest(){int infi=1;while(infi==1){}}
-    @Ignore
-    @Test
-    public void ignoreTest(){int infi=1;while(infi==1){}
-    }
-}
