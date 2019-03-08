ff664198a9612c3237b12e989176fcf0a5bcab31
Lungisa Lucky Mkhasakhasa <216060117@mycput.ac.za>
2019-03-08 13:46:07 +0200

    Using Maven as a build tool  i. Object Equality  ii. Object Identity  iii. Failing Test  iv.Timeouts  v. DisablingTest  (3)

--- src/main/java/za/ac/cput/Chapter3Asses/Rectangle.java
+++ src/main/java/za/ac/cput/Chapter3Asses/Rectangle.java
@@ -1,19 +1,12 @@
 package za.ac.cput.Chapter3Asses;
 public class Rectangle {
     private int width, length;
-    public Rectangle() {
-        this(0,0);
+    public Rectangle(){this(0,0);
     }
-    public Rectangle(int width, int length) {
-        this.width = width;
-        this.length = length;
+    public Rectangle(int width, int length) {this.width = width; this.length = length;
     }
-    public int area() {
-        int outPut=width * length;
-        return outPut;
+    public int area() {int outPut=width * length;return outPut;
     }
-    public int perimeter() {
-        int outPut=((width*2) +(length*2));
-        return outPut;
+    public int perimeter() {int outPut=((width*2) +(length*2));return outPut;
     }
 }
