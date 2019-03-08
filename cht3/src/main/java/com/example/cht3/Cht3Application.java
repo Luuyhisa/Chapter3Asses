ea0b0883ca72a54ee0d879eebdf2844ae88fe7c6
Lungisa Lucky Mkhasakhasa <216060117@mycput.ac.za>
2019-03-08 16:03:49 +0200

    Using Maven as a build tool  i. Object Equality  ii. Object Identity  iii. Failing Test  iv.Timeouts  v. DisablingTest  (4)

---
+++ cht3/src/main/java/com/example/cht3/Cht3Application.java
@@ -0,0 +1,13 @@
+package com.example.cht3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cht3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Cht3Application.class, args);
+    }
+
+}
